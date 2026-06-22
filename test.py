5aae3685ac006def5d308e898db5248534073375
Yasin Saberi <yassaberi97@gmail.com>
2026-06-22 21:31:40 +0330

    sql code completion for log table

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sqlalchemy as db
 import os
+import pandas as pd
 
 db_user = 'sa'
 db_password = os.getenv('MY_DB_PASSWORD')
@@ -11,3 +12,19 @@ connection_url = f"mssql+pyodbc://{db_user}:{db_password}@{db_host}:{db_port}/{d
 
 engine = db.create_engine(connection_url)
 
+log_query = """
+INSERT INTO PipelineLog (ExecutionStart, ExecutionEnd, RowsInserted, PipelineStatus)
+VALUES (:start, :end, :rows, :status);
+"""
+
+with engine.connect() as connection:
+    connection.execute(
+        db.text(log_query),
+        {
+            "start": start_time,
+            "end": end_time,
+            "rows": len(cleaned_df),
+            "status": "SUCCESS"
+        }
+    )
+    connection.commit()
